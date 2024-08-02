52ffb47033ef0b7367dcdd7e6a4f8d4cdaef5f2a
peterdudfield <peter.dudfield@hotmail.com>
2024-08-02 18:00:28 +0100

    update test PR comment

--- tests/select/test_select_time_slice.py
+++ tests/select/test_select_time_slice.py
@@ -3,12 +3,12 @@ from ocf_data_sampler.load.satellite import open_sat_data
 
 from datetime import timedelta
 import numpy as np
+import pandas as pd
 
 
 def test_select_time_slice(sat_zarr_path):
     sat = open_sat_data(sat_zarr_path)
-    print(sat)
-    t0 = sat.time_utc[3].values
+    t0 = pd.Timestamp(sat.time_utc[3].values)
 
     sat_sample = select_time_slice(
         ds=sat,
@@ -19,7 +19,9 @@ def test_select_time_slice(sat_zarr_path):
     )
 
     assert len(sat_sample.time_utc) == 3
-    assert sat_sample.time_utc[1] == t0
+    assert (sat_sample.time_utc == pd.date_range(
+        t0 - timedelta(minutes=5), t0 + timedelta(minutes=5), freq=timedelta(minutes=5)
+    )).all()
 
 
 # TODO could to test with intervals, but we might want to remove this functionaility
@@ -27,7 +29,7 @@ def test_select_time_slice(sat_zarr_path):
 
 def test_select_time_slice_out_of_bounds(sat_zarr_path):
     sat = open_sat_data(sat_zarr_path)
-    t0 = sat.time_utc[-1].values
+    t0 = pd.Timestamp(sat.time_utc[-1].values)
 
     sat_sample = select_time_slice(
         ds=sat,
@@ -38,12 +40,10 @@ def test_select_time_slice_out_of_bounds(sat_zarr_path):
         fill_selection=True,
     )
 
-    print(sat_sample.time_utc.values)
-
     assert len(sat_sample.time_utc) == 3
-    assert sat_sample.time_utc[1] == t0
+    assert (sat_sample.time_utc == pd.date_range(
+        t0 - timedelta(minutes=5), t0 + timedelta(minutes=5), freq=timedelta(minutes=5)
+    )).all()
     # Correct number of time steps are all NaN
     sat_sel = sat_sample.isel(x_geostationary=0, y_geostationary=0, channel=0)
     assert np.isnan(sat_sel.values).sum() == 1
-
-
