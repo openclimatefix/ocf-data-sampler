6d08e106c57bcd401b20a1bdf84b5a2b7ac23d98
Sukhil Patel <sukhil@Sukhils-MacBook-Pro.local>
2024-08-13 17:27:11 +0100

    Refactor function

--- ocf_data_sampler/select/fill_time_periods.py
+++ ocf_data_sampler/select/fill_time_periods.py
@@ -1,15 +1,12 @@
-"""Select time periods"""
+"""fill time periods"""
 
 import pandas as pd
 import numpy as np
 
 
-
 def fill_time_periods(time_periods: pd.DataFrame, freq: pd.Timedelta):
-    datetimes = []
-    for _, row in time_periods.iterrows():
-        start_dt = pd.Timestamp(row["start_dt"]).ceil(freq)
-        end_dt = pd.Timestamp(row["end_dt"])
-        datetimes.append(pd.date_range(start_dt, end_dt, freq=freq))
+    start_dts = time_periods["start_dt"].apply(lambda x: pd.Timestamp(x).ceil(freq))
+    end_dts = time_periods['end_dt'].apply(lambda x: pd.Timestamp(x))
+    date_ranges = [pd.date_range(start_dt, end_dt, freq=freq) for start_dt, end_dt in zip(start_dts, end_dts)]
 
-    return pd.DatetimeIndex(np.concatenate(datetimes))
+    return pd.DatetimeIndex(np.concatenate(date_ranges))
