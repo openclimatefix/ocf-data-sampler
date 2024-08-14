bded8db7291c50ccd8bf1a1d0a402b1c2c0750d1
Sukhil Patel <sukhil@Sukhils-MacBook-Pro.local>
2024-08-14 09:22:49 +0100

    Simplify end_dts

--- ocf_data_sampler/select/fill_time_periods.py
+++ ocf_data_sampler/select/fill_time_periods.py
@@ -5,8 +5,8 @@ import numpy as np
 
 
 def fill_time_periods(time_periods: pd.DataFrame, freq: pd.Timedelta):
-    start_dts = time_periods["start_dt"].apply(lambda x: pd.Timestamp(x).ceil(freq))
-    end_dts = time_periods['end_dt'].apply(lambda x: pd.Timestamp(x))
+    start_dts = pd.to_datetime(time_periods["start_dt"].values).ceil(freq)
+    end_dts = pd.to_datetime(time_periods['end_dt'])
     date_ranges = [pd.date_range(start_dt, end_dt, freq=freq) for start_dt, end_dt in zip(start_dts, end_dts)]
 
     return pd.DatetimeIndex(np.concatenate(date_ranges))
