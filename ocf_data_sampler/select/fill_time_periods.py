09d5e09af8dcbbbe804bb0f7bafe5d75aac4cd11
Sukhil Patel <sukhil@Sukhils-MacBook-Pro.local>
2024-08-14 11:48:56 +0100

    Make them look the same

--- ocf_data_sampler/select/fill_time_periods.py
+++ ocf_data_sampler/select/fill_time_periods.py
@@ -6,7 +6,6 @@ import numpy as np
 
 def fill_time_periods(time_periods: pd.DataFrame, freq: pd.Timedelta):
     start_dts = pd.to_datetime(time_periods["start_dt"].values).ceil(freq)
-    end_dts = pd.to_datetime(time_periods['end_dt'])
+    end_dts = pd.to_datetime(time_periods["end_dt"].values)
     date_ranges = [pd.date_range(start_dt, end_dt, freq=freq) for start_dt, end_dt in zip(start_dts, end_dts)]
-
     return pd.DatetimeIndex(np.concatenate(date_ranges))
