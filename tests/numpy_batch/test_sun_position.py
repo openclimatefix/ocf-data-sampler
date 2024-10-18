16184e9a632eec26ccea9edaecd84830230c8699
peterdudfield <peter.dudfield@hotmail.com>
2024-10-18 19:14:43 +0100

    update tests

--- tests/numpy_batch/test_sun_position.py
+++ tests/numpy_batch/test_sun_position.py
@@ -6,7 +6,7 @@ from ocf_data_sampler.numpy_batch.sun_position import (
     calculate_azimuth_and_elevation, make_sun_position_numpy_batch
 )
 
-from ocf_datapipes.batch import NumpyBatch, BatchKey
+from ocf_data_sampler.numpy_batch.gsp import GSPBatchKey
 
 
 @pytest.mark.parametrize("lat", [0, 5, 10, 23.5])
@@ -71,11 +71,11 @@ def test_make_sun_position_numpy_batch():
 
     batch = make_sun_position_numpy_batch(datetimes, lon, lat, key_preffix="gsp")
 
-    assert BatchKey.gsp_solar_elevation in batch
-    assert BatchKey.gsp_solar_azimuth in batch
+    assert GSPBatchKey.gsp_solar_elevation in batch
+    assert GSPBatchKey.gsp_solar_azimuth in batch
 
     # The solar coords are normalised in the function
-    assert (batch[BatchKey.gsp_solar_elevation]>=0).all()
-    assert (batch[BatchKey.gsp_solar_elevation]<=1).all()
-    assert (batch[BatchKey.gsp_solar_azimuth]>=0).all()
-    assert (batch[BatchKey.gsp_solar_azimuth]<=1).all()
+    assert (batch[GSPBatchKey.gsp_solar_elevation]>=0).all()
+    assert (batch[GSPBatchKey.gsp_solar_elevation]<=1).all()
+    assert (batch[GSPBatchKey.gsp_solar_azimuth]>=0).all()
+    assert (batch[GSPBatchKey.gsp_solar_azimuth]<=1).all()
