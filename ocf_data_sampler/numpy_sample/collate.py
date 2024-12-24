99f622e2d15b2eaf11327879c720b72e11cda0b7
Ajani Timothy <timothyinioluwa@gmail.com>
2024-12-24 22:27:07 +0100

    docs: add docstring to validate input list in stack_np_examples_into_sample

--- ocf_data_sampler/numpy_sample/collate.py
+++ ocf_data_sampler/numpy_sample/collate.py
@@ -9,6 +9,18 @@ logger = logging.getLogger(__name__)
 
 
 def stack_np_examples_into_sample(dict_list):
+#     """
+#     Stacks Numpy examples into a sample
+
+#     See also: `unstack_np_sample_into_examples()` for opposite
+
+#     Args:
+#         dict_list: A list of dict-like Numpy examples to stack
+
+#     Returns:
+#         The stacked NumpySample object
+#     """
+
     if not dict_list:
         raise ValueError("Input is empty")
 
@@ -49,55 +61,6 @@ def stack_nested_keys(dict_list, parent_key, child_key):
         )
         for key in nested_keys
     }
-
-
-# def stack_np_examples_into_sample(dict_list):
-#     """
-#     Stacks Numpy examples into a sample
-
-#     See also: `unstack_np_sample_into_examples()` for opposite
-
-#     Args:
-#         dict_list: A list of dict-like Numpy examples to stack
-
-#     Returns:
-#         The stacked NumpySample object
-#     """
-#     sample = {}
-
-#     sample_keys = list(dict_list[0].keys())
-
-#     for sample_key in sample_keys:
-#         # NWP is nested so treat separately
-#         if sample_key == "nwp":
-#             nwp_sample: dict[str, NWPSampleKey] = {}
-
-#             # Unpack source keys
-#             nwp_sources = list(dict_list[0]["nwp"].keys())
-
-#             for nwp_source in nwp_sources:
-#                 # Keys can be different for different NWPs
-#                 nwp_sample_keys = list(dict_list[0]["nwp"][nwp_source].keys())
-
-#                 nwp_source_sample = {}
-#                 for nwp_sample_key in nwp_sample_keys:
-#                     nwp_source_sample[nwp_sample_key] = stack_data_list(
-#                         [d["nwp"][nwp_source][nwp_sample_key] for d in dict_list],
-#                         nwp_sample_key,
-#                     )
-
-#                 nwp_sample[nwp_source] = nwp_source_sample
-
-#             sample["nwp"] = nwp_sample
-
-#         else:
-#             sample[sample_key] = stack_data_list(
-#                 [d[sample_key] for d in dict_list],
-#                 sample_key,
-#             )
-
-#     return sample
-
 
 def _key_is_constant(sample_key):
     is_constant = sample_key.endswith("t0_idx") or sample_key == NWPSampleKey.channel_names
