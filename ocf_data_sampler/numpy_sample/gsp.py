fd357ab9216043cd4f8db08a6f1bab8385acc357
Ajani Timothy <timothyinioluwa@gmail.com>
2025-01-03 19:44:34 +0100

    refactor: removed redundant none checks in  and

--- ocf_data_sampler/numpy_sample/gsp.py
+++ ocf_data_sampler/numpy_sample/gsp.py
@@ -21,23 +21,11 @@ def convert_gsp_to_numpy_sample(da: xr.DataArray, t0_idx: int | None = None) -> 
     """Convert from Xarray to NumpySample"""
 
    # Extract values from the DataArray
-    input_variables = {
-        'gsp': da.values,
-        'nominal_capacity_mwp': da.isel(time_utc=0)["nominal_capacity_mwp"].values,
-        'effective_capacity_mwp': da.isel(time_utc=0)["effective_capacity_mwp"].values,
-        'time_utc': da["time_utc"].values.astype(float),
-    }
-
-    # Check for None values and raise error with specific variable name if value is not passed or none
-    for var, value in input_variables.items():
-        if value is None:
-            raise ValueError(f"The variable '{var}' has a None value and is required.")
-
     example = {
-        GSPSampleKey.gsp: input_variables.get('gsp'),
-        GSPSampleKey.nominal_capacity_mwp:input_variables.get('nominal_capacity_mwp'),
-        GSPSampleKey.effective_capacity_mwp:input_variables.get('effective_capacity_mwp'),
-        GSPSampleKey.time_utc:  input_variables.get('time_utc'),
+        GSPSampleKey.gsp: da.values,
+        GSPSampleKey.nominal_capacity_mwp: da.isel(time_utc=0)["nominal_capacity_mwp"].values,
+        GSPSampleKey.effective_capacity_mwp: da.isel(time_utc=0)["effective_capacity_mwp"].values,
+        GSPSampleKey.time_utc: da["time_utc"].values.astype(float),
     }
 
     if t0_idx is not None:
