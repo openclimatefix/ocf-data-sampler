f3285063517c032381ecbe7a3ae75867a468fda4
Peter Dudfield <34686298+peterdudfield@users.noreply.github.com>
2025-01-14 09:23:01 +0000

    first try at adding site torch dataset presaved samples support + tests (#102)

--- ocf_data_sampler/torch_datasets/process_and_combine.py
+++ ocf_data_sampler/torch_datasets/process_and_combine.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import xarray as xr
-from typing import Tuple
+from typing import Optional
 
 from ocf_data_sampler.config import Configuration
 from ocf_data_sampler.constants import NWP_MEANS, NWP_STDS,RSS_MEAN,RSS_STD
@@ -21,8 +21,8 @@ from ocf_data_sampler.utils import minutes
 def process_and_combine_datasets(
     dataset_dict: dict,
     config: Configuration,
-    t0: pd.Timestamp,
-    location: Location,
+    t0: Optional[pd.Timestamp] = None,
+    location: Optional[Location] = None,
     target_key: str = 'gsp'
 ) -> dict:
 
@@ -96,50 +96,6 @@ def process_and_combine_datasets(
 
     return combined_sample
 
-
-def process_and_combine_site_sample_dict(
-    dataset_dict: dict,
-    config: Configuration,
-) -> xr.Dataset:
-    """
-    Normalize and combine data into a single xr Dataset
-
-    Args:
-        dataset_dict: dict containing sliced xr DataArrays
-        config: Configuration for the model
-
-    Returns:
-        xr.Dataset: A merged Dataset with nans filled in.
-    
-    """
-
-    data_arrays = []
-
-    if "nwp" in dataset_dict:
-        for nwp_key, da_nwp in dataset_dict["nwp"].items():
-            # Standardise
-            provider = config.input_data.nwp[nwp_key].provider
-            da_nwp = (da_nwp - NWP_MEANS[provider]) / NWP_STDS[provider]
-            data_arrays.append((f"nwp-{provider}", da_nwp))
-          
-    if "sat" in dataset_dict:
-        # Standardise
-        da_sat = dataset_dict["sat"]
-        da_sat = (da_sat - RSS_MEAN) / RSS_STD
-        data_arrays.append(("satellite", da_sat))
-
-    if "site" in dataset_dict:
-        # site_config = config.input_data.site
-        da_sites = dataset_dict["site"]
-        da_sites = da_sites / da_sites.capacity_kwp
-        data_arrays.append(("sites", da_sites))
-    
-    combined_sample_dataset = merge_arrays(data_arrays)
-
-    # Fill any nan values
-    return combined_sample_dataset.fillna(0.0)
-
-
 def merge_dicts(list_of_dicts: list[dict]) -> dict:
     """Merge a list of dictionaries into a single dictionary"""
     # TODO: This doesn't account for duplicate keys, which will be overwritten
@@ -147,61 +103,6 @@ def merge_dicts(list_of_dicts: list[dict]) -> dict:
     for d in list_of_dicts:
         combined_dict.update(d)
     return combined_dict
-
-
-def merge_arrays(normalised_data_arrays: list[Tuple[str, xr.DataArray]]) -> xr.Dataset:
-    """
-    Combine a list of DataArrays into a single Dataset with unique naming conventions.
-
-    Args:
-        list_of_arrays: List of tuples where each tuple contains:
-            - A string (key name).
-            - An xarray.DataArray.
-
-    Returns:
-        xr.Dataset: A merged Dataset with uniquely named variables, coordinates, and dimensions.
-    """
-    datasets = []
-
-    for key, data_array in normalised_data_arrays:
-        # Ensure all attributes are strings for consistency
-        data_array = data_array.assign_attrs(
-            {attr_key: str(attr_value) for attr_key, attr_value in data_array.attrs.items()}
-        )
-
-        # Convert DataArray to Dataset with the variable name as the key
-        dataset = data_array.to_dataset(name=key)
-
-        # Prepend key name to all dimension and coordinate names for uniqueness
-        dataset = dataset.rename(
-            {dim: f"{key}__{dim}" for dim in dataset.dims if dim not in dataset.coords}
-        )
-        dataset = dataset.rename(
-            {coord: f"{key}__{coord}" for coord in dataset.coords}
-        )
-
-        # Handle concatenation dimension if applicable
-        concat_dim = (
-            f"{key}__target_time_utc" if f"{key}__target_time_utc" in dataset.coords
-            else f"{key}__time_utc"
-        )
-
-        if f"{key}__init_time_utc" in dataset.coords:
-            init_coord = f"{key}__init_time_utc"
-            if dataset[init_coord].ndim == 0:  # Check if scalar
-                expanded_init_times = [dataset[init_coord].values] * len(dataset[concat_dim])
-                dataset = dataset.assign_coords({init_coord: (concat_dim, expanded_init_times)})
-
-        datasets.append(dataset)
-
-    # Ensure all datasets are valid xarray.Dataset objects
-    for ds in datasets:
-        assert isinstance(ds, xr.Dataset), f"Object is not an xr.Dataset: {type(ds)}"
-
-    # Merge all prepared datasets
-    combined_dataset = xr.merge(datasets)
-
-    return combined_dataset
 
 def fill_nans_in_arrays(sample: dict) -> dict:
     """Fills all NaN values in each np.ndarray in the sample dictionary with zeros.
