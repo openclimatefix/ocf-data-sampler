6e99cba0dbf245cce5419d2d75ddcc53d8694d61
Sukh-P <sukhil@hotmail.co.uk>
2025-01-22 18:44:56 +0000

    Add date & times

--- ocf_data_sampler/numpy_sample/site.py
+++ ocf_data_sampler/numpy_sample/site.py
@@ -9,11 +9,14 @@ class SiteSampleKey:
     capacity_kwp = "site_capacity_kwp"
     time_utc = "site_time_utc"
     t0_idx = "site_t0_idx"
+    id = "site_id"
     solar_azimuth = "site_solar_azimuth"
     solar_elevation = "site_solar_elevation"
-    id = "site_id"
+    date_sin = "site_date_sin"
+    date_cos = "site_date_cos"
+    time_sin = "site_time_sin"
+    time_cos = "site_time_cos"
 
-# TODO update to include trig datetime + solar coords
 def convert_site_to_numpy_sample(da: xr.DataArray, t0_idx: int | None = None) -> dict:
     """Convert from Xarray to NumpySample"""
 
@@ -23,6 +26,10 @@ def convert_site_to_numpy_sample(da: xr.DataArray, t0_idx: int | None = None) ->
         SiteSampleKey.capacity_kwp: da.isel(time_utc=0)["capacity_kwp"].values,
         SiteSampleKey.time_utc: da["time_utc"].values.astype(float),
         SiteSampleKey.id: da["site_id"].values,
+        SiteSampleKey.date_sin: da["date_sin"].values,
+        SiteSampleKey.date_cos: da["date_cos"].values,
+        SiteSampleKey.time_sin: da["time_sin"].values,
+        SiteSampleKey.time_cos: da["time_cos"].values,
     }
 
     if t0_idx is not None:
