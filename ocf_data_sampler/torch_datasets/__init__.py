936148b3c2e064f7a54af9290b84b1142850a895
Felix Peretz <felix@openclimatefix.org>
2025-01-27 13:47:58 +0000

    Torch datasets init update

--- ocf_data_sampler/torch_datasets/__init__.py
+++ ocf_data_sampler/torch_datasets/__init__.py
@@ -1,2 +1,13 @@
 from .pvnet_uk_regional import PVNetUKRegionalDataset
-from .site import SitesDataset
+
+from .site import (
+    convert_netcdf_to_numpy_sample,
+    convert_from_dataset_to_dict_datasets,
+    SitesDataset
+)
+
+__all__ = [
+    'convert_netcdf_to_numpy_sample',
+    'convert_from_dataset_to_dict_datasets',
+    'SitesDataset'
+]
