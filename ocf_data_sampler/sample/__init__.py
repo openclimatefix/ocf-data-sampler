2aa45a570c6ca183fac05c325cad145be8b54467
Felix <137530077+felix-e-h-p@users.noreply.github.com>
2025-01-29 09:44:26 +0000

    Update __init__.py

--- ocf_data_sampler/sample/__init__.py
+++ ocf_data_sampler/sample/__init__.py
@@ -1,5 +1,3 @@
-# __init__.py
-
 from ocf_data_sampler.sample.base import SampleBase
 from ocf_data_sampler.sample.uk_regional import UKRegionalSample
 from ocf_data_sampler.sample.site import SiteSample
