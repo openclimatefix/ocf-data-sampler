45d42945daa7befedad9a0a9df524e3a513e3e91
Chaitanya Reddy Palem <cr5638963@gmail.com>
2025-02-07 23:49:53 +0530

    ISSUE NO - 13 Resloved (#155)
    * ISSUE NO - 13 Resloved
    
    * Fixes #13: Remove wildcard support for GCP paths, update docstrings
    
    * ocf_data_sampler/data/uk_gsp_locations.csv-updated
    
    * updated the comments
    
    * updated Doc Strings
    
    * updated Doc-strings
    
    * removed Test data
    
    ---------
    
    Co-authored-by: chaitanya reddy palem <chaitanyaRDY>

--- ocf_data_sampler/load/satellite.py
+++ ocf_data_sampler/load/satellite.py
@@ -12,15 +12,20 @@ from ocf_data_sampler.load.utils import (
 
 
 def _get_single_sat_data(zarr_path: Path | str) -> xr.Dataset:
-    """Helper function to open a zarr from either local or GCP path.
-
-    The local or GCP path may contain wildcard matching (*)
+    """Helper function to open a Zarr from either a local or GCP path.
 
     Args:
-        zarr_path: Path to zarr file
+        zarr_path: Path to a Zarr file. Wildcards (*) are supported **only** for local paths. 
+                   GCS paths (gs://) **do not support** wildcards.
+
+    Returns:
+        An xarray Dataset containing satellite data.
+
+    Raises:
+        ValueError: If a wildcard (*) is used in a GCS (gs://) path.
     """
 
-    # These kwargs are used if zarr path contains "*"
+    # These kwargs are used if the path contains "*"
     openmf_kwargs = dict(
         engine="zarr",
         concat_dim="time",
@@ -29,19 +34,17 @@ def _get_single_sat_data(zarr_path: Path | str) -> xr.Dataset:
         join="override",
     )
 
-    # Need to generate list of files if using GCP bucket storage
+    # Raise an error if a wildcard is used in a GCP path
     if "gs://" in str(zarr_path) and "*" in str(zarr_path):
-        result_string = subprocess.run(
-            f"gsutil ls -d {zarr_path}".split(" "), stdout=subprocess.PIPE
-        ).stdout.decode("utf-8")
-        files = result_string.splitlines()
+        raise ValueError("Wildcard (*) paths are not supported for GCP (gs://) URLs.")
 
-        ds = xr.open_mfdataset(files, **openmf_kwargs)
-
-    elif "*" in str(zarr_path):  # Multi-file dataset
+    # Handle multi-file dataset for local paths
+    if "*" in str(zarr_path):
         ds = xr.open_mfdataset(zarr_path, **openmf_kwargs)
     else:
         ds = xr.open_dataset(zarr_path, engine="zarr", chunks="auto")
+
+    # Ensure time is unique and sorted
     ds = ds.drop_duplicates("time").sortby("time")
 
     return ds
@@ -53,24 +56,6 @@ def open_sat_data(zarr_path: Path | str | list[Path] | list[str]) -> xr.DataArra
     Args:
       zarr_path: Cloud URL or local path pattern, or list of these. If GCS URL, it must start with
           'gs://'.
-
-    Example:
-        With wild cards and GCS path:
-        ```
-        zarr_paths = [
-            "gs://bucket/2020_nonhrv_split_*.zarr",
-            "gs://bucket/2019_nonhrv_split_*.zarr",
-        ]
-        ds = open_sat_data(zarr_paths)
-        ```
-        Without wild cards and with local path:
-        ```
-        zarr_paths = [
-            "/data/2020_nonhrv.zarr",
-            "/data/2019_nonhrv.zarr",
-        ]
-        ds = open_sat_data(zarr_paths)
-        ```
     """
 
     # Open the data
@@ -84,7 +69,7 @@ def open_sat_data(zarr_path: Path | str | list[Path] | list[str]) -> xr.DataArra
     else:
         ds = _get_single_sat_data(zarr_path)
 
-    # Rename
+    # Rename dimensions
     ds = ds.rename(
         {
             "variable": "channel",
@@ -92,13 +77,13 @@ def open_sat_data(zarr_path: Path | str | list[Path] | list[str]) -> xr.DataArra
         }
     )
 
-    # Check the timestamps are unique and increasing
+    # Check timestamps
     check_time_unique_increasing(ds.time_utc)
 
-    # Make sure the spatial coords are in increasing order
+    # Ensure spatial coordinates are sorted
     ds = make_spatial_coords_increasing(ds, x_coord="x_geostationary", y_coord="y_geostationary")
 
     ds = ds.transpose("time_utc", "channel", "x_geostationary", "y_geostationary")
-
     # TODO: should we control the dtype of the DataArray?
+
     return get_xr_data_array_from_xr_dataset(ds)
