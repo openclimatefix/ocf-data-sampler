fd17bd606519047afa50b72426ffe2f8d2927ab7
gabrielelibardi <gabrielelibardi@yahoo.it>
2025-02-09 18:22:39 +0100

    open_icon_global removed

--- ocf_data_sampler/load/nwp/providers/icon.py
+++ ocf_data_sampler/load/nwp/providers/icon.py
@@ -46,31 +46,3 @@ def open_icon_eu(zarr_path) -> xr.Dataset:
     nwp = nwp.transpose('init_time_utc', 'step', 'channel', 'latitude', 'longitude')
     nwp = make_spatial_coords_increasing(nwp, x_coord="longitude", y_coord="latitude")
     return nwp
-
-
-def open_icon_global(zarr_path) -> xr.Dataset:
-    """
-    Opens the ICON data
-
-    ICON Global Data is on an isohedral grid, so the points are not regularly spaced
-    It has data on multiple pressure levels, as well as the surface
-    Each of the variables is its own data variable
-
-    Args:
-        zarr_path: Path to the zarr to open
-
-    Returns:
-        Xarray DataArray of the NWP data
-    """
-    # Open the data
-    nwp = open_zarr_paths(zarr_path, time_dim="time")
-    nwp = nwp.rename({"time": "init_time_utc"})
-    # ICON Global archive script didn't define the values to be
-    # associated with lat/lon so fixed here
-    nwp.coords["latitude"] = (("values",), nwp.latitude.values)
-    nwp.coords["longitude"] = (("values",), nwp.longitude.values)
-    # Sanity checks.
-    time = pd.DatetimeIndex(nwp.init_time_utc)
-    assert time.is_unique
-    assert time.is_monotonic_increasing
-    return nwp
