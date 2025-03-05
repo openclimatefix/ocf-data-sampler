acf7dd1706c4d16a652b8d88e36c247cc15e3e6a
Siddharth <siddharth899@gmail.com>
2025-03-05 21:38:53 +0530

    Removing redundant comment

--- ocf_data_sampler/load/nwp/nwp.py
+++ ocf_data_sampler/load/nwp/nwp.py
@@ -3,7 +3,7 @@
 import xarray as xr
 
 from ocf_data_sampler.load.nwp.providers.ecmwf import open_ifs
-from ocf_data_sampler.load.nwp.providers.gfs import open_gfs  # Import GFS provider
+from ocf_data_sampler.load.nwp.providers.gfs import open_gfs
 from ocf_data_sampler.load.nwp.providers.icon import open_icon_eu
 from ocf_data_sampler.load.nwp.providers.ukv import open_ukv
 
