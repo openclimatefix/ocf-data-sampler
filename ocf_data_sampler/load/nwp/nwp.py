9d81b838032fd54901d5512a4c9e6c99ac06a8d5
James Fulton <djamesfulton@yahoo.co.uk>
2025-03-13 16:48:00 +0000

    add cloudcasting loader

--- ocf_data_sampler/load/nwp/nwp.py
+++ ocf_data_sampler/load/nwp/nwp.py
@@ -6,6 +6,7 @@ from ocf_data_sampler.load.nwp.providers.ecmwf import open_ifs
 from ocf_data_sampler.load.nwp.providers.gfs import open_gfs
 from ocf_data_sampler.load.nwp.providers.icon import open_icon_eu
 from ocf_data_sampler.load.nwp.providers.ukv import open_ukv
+from ocf_data_sampler.load.nwp.providers.cloudcasting import open_cloudcasting
 
 
 def open_nwp(zarr_path: str | list[str], provider: str) -> xr.DataArray:
@@ -28,6 +29,8 @@ def open_nwp(zarr_path: str | list[str], provider: str) -> xr.DataArray:
         _open_nwp = open_icon_eu
     elif provider == "gfs":
         _open_nwp = open_gfs
+    elif provider == "cloudcasting":
+        _open_nwp = open_cloudcasting
     else:
         raise ValueError(f"Unknown provider: {provider}")
 
