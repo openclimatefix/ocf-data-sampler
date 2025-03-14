69a88ccce7c22b18acf3b54485e65386f468ad33
James Fulton <41546094+dfulu@users.noreply.github.com>
2025-03-14 19:50:10 +0000

    Add cloudcasting as NWP provider (#215)

--- ocf_data_sampler/load/nwp/nwp.py
+++ ocf_data_sampler/load/nwp/nwp.py
@@ -2,6 +2,7 @@
 
 import xarray as xr
 
+from ocf_data_sampler.load.nwp.providers.cloudcasting import open_cloudcasting
 from ocf_data_sampler.load.nwp.providers.ecmwf import open_ifs
 from ocf_data_sampler.load.nwp.providers.gfs import open_gfs
 from ocf_data_sampler.load.nwp.providers.icon import open_icon_eu
@@ -28,6 +29,8 @@ def open_nwp(zarr_path: str | list[str], provider: str) -> xr.DataArray:
         _open_nwp = open_icon_eu
     elif provider == "gfs":
         _open_nwp = open_gfs
+    elif provider == "cloudcasting":
+        _open_nwp = open_cloudcasting
     else:
         raise ValueError(f"Unknown provider: {provider}")
 
---
+++ ocf_data_sampler/load/nwp/providers/cloudcasting.py
@@ -0,0 +1,51 @@
+"""Cloudcasting provider loader."""
+
+from pathlib import Path
+
+import xarray as xr
+
+from ocf_data_sampler.load.nwp.providers.utils import open_zarr_paths
+from ocf_data_sampler.load.utils import (
+    check_time_unique_increasing,
+    get_xr_data_array_from_xr_dataset,
+    make_spatial_coords_increasing,
+)
+
+
+def open_cloudcasting(zarr_path: Path | str | list[Path] | list[str]) -> xr.DataArray:
+    """Opens the satellite predictions from cloudcasting.
+
+    Cloudcasting is a OCF forecast product. We forecast future satellite images from recent
+    satellite images. More information can be found in the references below.
+
+    Args:
+        zarr_path: Path to the zarr to open
+
+    Returns:
+        Xarray DataArray of the cloudcasting data
+
+    References:
+            [1] https://www.openclimatefix.org/projects/cloud-forecasting
+            [2] https://github.com/ClimeTrend/cloudcasting
+            [3] https://github.com/openclimatefix/sat_pred
+    """
+    # Open the data
+    ds = open_zarr_paths(zarr_path)
+
+    # Rename
+    ds = ds.rename(
+        {
+            "init_time": "init_time_utc",
+            "variable": "channel",
+        },
+    )
+
+    # Check the timestamps are unique and increasing
+    check_time_unique_increasing(ds.init_time_utc)
+
+    # Make sure the spatial coords are in increasing order
+    ds = make_spatial_coords_increasing(ds, x_coord="x_geostationary", y_coord="y_geostationary")
+
+    ds = ds.transpose("init_time_utc", "step", "channel", "x_geostationary", "y_geostationary")
+
+    return get_xr_data_array_from_xr_dataset(ds)
