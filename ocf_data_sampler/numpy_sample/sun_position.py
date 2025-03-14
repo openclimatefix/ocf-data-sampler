bb3670db17abdc7939e6ec4ade58c54eb57e2e39
Siddharth <siddharth899@gmail.com>
2025-03-14 17:02:04 +0530

    Added NumpySample for make_sun_position_numpy_sample

--- ocf_data_sampler/numpy_sample/sun_position.py
+++ ocf_data_sampler/numpy_sample/sun_position.py
@@ -3,6 +3,7 @@
 import numpy as np
 import pandas as pd
 import pvlib
+from ocf_data_sampler.numpy_sample.common_types import NumpySample
 
 def calculate_azimuth_and_elevation(
     datetimes: pd.DatetimeIndex,
@@ -34,7 +35,7 @@ def make_sun_position_numpy_sample(
     datetimes: pd.DatetimeIndex,
     lon: float,
     lat: float,
-) -> dict:
+) -> NumpySample:
     """Creates NumpySample with standardized solar coordinates.
 
     Args:
