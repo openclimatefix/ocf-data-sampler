48e76d938df01264936008aee4933848a7c2ef66
Alexandra Udaltsova <43303448+AUdaltsova@users.noreply.github.com>
2025-03-28 18:05:37 +0000

    Update ecmwf.py

--- ocf_data_sampler/load/nwp/providers/ecmwf.py
+++ ocf_data_sampler/load/nwp/providers/ecmwf.py
@@ -1,20 +1,18 @@
-"""ECMWF provider loaders."""
-
+"""ECMWF provider loaders"""
 import xarray as xr
-
 from ocf_data_sampler.load.nwp.providers.utils import open_zarr_paths
 from ocf_data_sampler.load.utils import (
     check_time_unique_increasing,
-    get_xr_data_array_from_xr_dataset,
     make_spatial_coords_increasing,
     get_xr_data_array_from_xr_dataset
 )
 
-MEANS_PATH = "/home/alex/from-sol/europe.zarr"
+MEANS_PATH = "PLACEHOLDER"
 
 
 def open_ifs(zarr_path: str | list[str], ensemble_member: int | None) -> xr.DataArray:
-    """Opens the ECMWF IFS NWP data.
+    """
+    Opens the ECMWF IFS NWP data
 
     Args:
         zarr_path: Path to the zarr to open
@@ -22,45 +20,19 @@ def open_ifs(zarr_path: str | list[str], ensemble_member: int | None) -> xr.Data
     Returns:
         Xarray DataArray of the NWP data
     """
+    # Open the data
     ds = open_zarr_paths(zarr_path)
 
-    ds = ds.rename_vars({'cloud_cover_high': 'hcc', 'cloud_cover_low': 'lcc', 
-                                         'cloud_cover_medium': 'mcc', 
-                                         'cloud_cover_total': 'tcc',
-                                         'downward_longwave_radiation_flux_gl': 'dlwrf', 
-                                         'downward_shortwave_radiation_flux_gl': 'dswrf', 
-                                         'downward_ultraviolet_radiation_flux_gl': 'duvrs', 
-                                         'direct_shortwave_radiation_flux_gl': 'sr',
-                                         'snow_depth_gl': 'sde', 'temperature_sl': 't2m', 
-                                         'total_precipitation_rate_gl': 'prate', 'visibility_sl': 'vis', 
-                                         'wind_u_component_100m': 'u100', 'wind_u_component_10m': 'u10', 
-                                         'wind_u_component_200m': 'u200', 'wind_v_component_100m': 'v100', 
-                                         'wind_v_component_10m': 'v10', 'wind_v_component_200m': 'v200'})
+    ds = rename_to_legacy_variables(ds)
     ds = ds.to_dataarray(dim='variable')
 
 
     # WP6 hack
     if ensemble_member:
-        ds = ds.rename_vars({'cloud_cover_low': 'lcc', 'direct_shortwave_radiation_flux_gl': 'sr', 
-                            'downward_longwave_radiation_flux_gl': 'dlwrf',
-                            'downward_shortwave_radiation_flux_gl': 'dswrf', 
-                            'downward_ultraviolet_radiation_flux_gl': 'duvrs', 'temperature_sl': 't2m'})
-        ds = ds.to_dataarray(dim='variable')
         ds = ds.sel(ensemble_member = ensemble_member)
-
+        
         means_ds = open_zarr_paths(MEANS_PATH)
-        means_ds = means_ds.rename_vars({'cloud_cover_high': 'hcc', 'cloud_cover_low': 'lcc', 
-                                         'cloud_cover_medium': 'mcc', 
-                                         'cloud_cover_total': 'tcc',
-                                         'downward_longwave_radiation_flux_gl': 'dlwrf', 
-                                         'downward_shortwave_radiation_flux_gl': 'dswrf', 
-                                         'downward_ultraviolet_radiation_flux_gl': 'duvrs', 
-                                         'direct_shortwave_radiation_flux_gl': 'sr',
-                                         'snow_depth_gl': 'sde', 'temperature_sl': 't2m', 
-                                         'total_precipitation_rate_gl': 'prate', 'visibility_sl': 'vis', 
-                                         'wind_u_component_100m': 'u100', 'wind_u_component_10m': 'u10', 
-                                         'wind_u_component_200m': 'u200', 'wind_v_component_100m': 'v100', 
-                                         'wind_v_component_10m': 'v10', 'wind_v_component_200m': 'v200'})
+        means_ds = rename_to_legacy_variables(means_ds)
         means_ds = means_ds.to_dataarray(dim='variable')
         means_ds = means_ds.sel(
             init_time = ds.init_time, 
@@ -89,4 +61,29 @@ def open_ifs(zarr_path: str | list[str], ensemble_member: int | None) -> xr.Data
     ds = ds.transpose("init_time_utc", "step", "channel", "longitude", "latitude")
     
     # TODO: should we control the dtype of the DataArray?
-    return get_xr_data_array_from_xr_dataset(ds)
+    # return get_xr_data_array_from_xr_dataset(ds)
+    return ds
+
+
+def rename_to_legacy_variables(ds):
+    renaming_dict = {
+        'cloud_cover_high': 'hcc', 'cloud_cover_low': 'lcc', 
+        'cloud_cover_medium': 'mcc', 
+        'cloud_cover_total': 'tcc',
+        'downward_longwave_radiation_flux_gl': 'dlwrf', 
+        'downward_shortwave_radiation_flux_gl': 'dswrf', 
+        'downward_ultraviolet_radiation_flux_gl': 'duvrs', 
+        'direct_shortwave_radiation_flux_gl': 'sr',
+        'snow_depth_gl': 'sde', 'temperature_sl': 't2m', 
+        'total_precipitation_rate_gl': 'prate', 'visibility_sl': 'vis', 
+        'wind_u_component_100m': 'u100', 'wind_u_component_10m': 'u10', 
+        'wind_u_component_200m': 'u200', 'wind_v_component_100m': 'v100', 
+        'wind_v_component_10m': 'v10', 'wind_v_component_200m': 'v200'
+        }
+    
+    subset_renaming_dict = renaming_dict.copy()
+    for key in renaming_dict:
+        if key not in ds:
+            del subset_renaming_dict[key]
+
+    return ds.rename_vars(subset_renaming_dict)
