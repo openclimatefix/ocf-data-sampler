5a0499e005fbb94e5f992c6a9c18700f42e790b1
Dakshbir <144359831+Dakshbir@users.noreply.github.com>
2025-04-01 12:37:08 +0530

    initial changes

--- ocf_data_sampler/numpy_sample/datetime_features.py
+++ ocf_data_sampler/numpy_sample/datetime_features.py
@@ -24,15 +24,15 @@ def _get_date_time_in_pi(dt: pd.DatetimeIndex) -> tuple[np.ndarray, np.ndarray]:
     return date_in_pi, time_in_pi
 
 
-def make_datetime_numpy_dict(datetimes: pd.DatetimeIndex, key_prefix: str = "wind") -> NumpySample:
+def make_datetime_numpy_dict(datetimes: pd.DatetimeIndex) -> NumpySample:
     """Creates dictionary of cyclical datetime features - encoded."""
     date_in_pi, time_in_pi = _get_date_time_in_pi(datetimes)
 
     time_numpy_sample = {}
 
-    time_numpy_sample[key_prefix + "_date_sin"] = np.sin(date_in_pi)
-    time_numpy_sample[key_prefix + "_date_cos"] = np.cos(date_in_pi)
-    time_numpy_sample[key_prefix + "_time_sin"] = np.sin(time_in_pi)
-    time_numpy_sample[key_prefix + "_time_cos"] = np.cos(time_in_pi)
+    time_numpy_sample["date_sin"] = np.sin(date_in_pi)
+    time_numpy_sample["date_cos"] = np.cos(date_in_pi)
+    time_numpy_sample["time_sin"] = np.sin(time_in_pi)
+    time_numpy_sample["time_cos"] = np.cos(time_in_pi)
 
     return time_numpy_sample
