2727ae37ebd8f1cf8ba5407bb1b14b87732b1f45
Sukhil Patel <42407101+Sukh-P@users.noreply.github.com>
2025-05-19 15:30:25 +0100

    Update dropout functions (#261)

--- tests/select/test_dropout.py
+++ tests/select/test_dropout.py
@@ -3,7 +3,7 @@ import pandas as pd
 import pytest
 import xarray as xr
 
-from ocf_data_sampler.select.dropout import apply_dropout_time, draw_dropout_time
+from ocf_data_sampler.select.dropout import apply_sampled_dropout_time
 
 
 @pytest.fixture(scope="module")
@@ -21,49 +21,66 @@ def da_sample():
     return da_sat
 
 
-def test_draw_dropout_time():
-    t0 = pd.Timestamp("2021-01-01 04:00:00")
+def test_apply_sampled_dropout_time_multiple_timedeltas(da_sample):
+    t0 = pd.Timestamp("2024-01-01 13:00:00")
 
-    dropout_timedeltas = pd.to_timedelta([-30, -60], unit="min")
-    dropout_time = draw_dropout_time(t0, dropout_timedeltas, dropout_frac=1)
+    dropout_timedeltas = pd.to_timedelta([-30, -45], unit="min")
+    da_sample_dropout = apply_sampled_dropout_time(
+        t0,
+        dropout_timedeltas=dropout_timedeltas,
+        dropout_frac=1,
+        da=da_sample,
+    )
 
-    assert isinstance(dropout_time, pd.Timestamp)
-    assert (dropout_time - t0) in dropout_timedeltas
+    latest_expected_cut_off = t0 + pd.Timedelta(-30, "min")
 
-
-def test_draw_dropout_time_partial():
-    t0 = pd.Timestamp("2021-01-01 04:00:00")
-
-    dropout_timedeltas = pd.to_timedelta([-30, -60], unit="min")
-
-    dropouts = set()
-
-    # Loop over 1000 to have very high probability of seeing all dropouts
-    # The chances of this failing by chance are approx ((2/3)^100)*3 = 7e-18
-    for _ in range(100):
-        dropouts.add(draw_dropout_time(t0, dropout_timedeltas, dropout_frac=2 / 3))
-
-    # TODO: Check all dropouts are present
+    assert (
+        da_sample_dropout.sel(
+            time_utc=slice(latest_expected_cut_off + pd.Timedelta(5, "min"), None),
+        )
+        .isnull()
+        .all()
+    )
 
 
-def test_draw_dropout_time_none():
+def test_apply_sampled_dropout_time_none(da_sample):
     t0 = pd.Timestamp("2021-01-01 04:00:00")
 
     # Dropout fraction is 0
     dropout_timedeltas = [pd.Timedelta(-30, "min")]
-    dropout_time = draw_dropout_time(t0, dropout_timedeltas=dropout_timedeltas, dropout_frac=0)
-    assert dropout_time is None
+    da_sample_dropout = apply_sampled_dropout_time(
+        t0,
+        dropout_timedeltas=dropout_timedeltas,
+        dropout_frac=0,
+        da=da_sample,
+    )
+
+    # Check data arrays are equal as dropout time would be None
+    xr.testing.assert_equal(da_sample_dropout, da_sample)
 
     # No dropout timedeltas and dropout fraction is 0
-    dropout_time = draw_dropout_time(t0, dropout_timedeltas=[], dropout_frac=0)
-    assert dropout_time is None
+    da_sample_dropout = apply_sampled_dropout_time(
+        t0,
+        dropout_timedeltas=[],
+        dropout_frac=0,
+        da=da_sample,
+    )
+
+    # Check data arrays are equal as dropout time would be None
+    xr.testing.assert_equal(da_sample_dropout, da_sample)
 
 
-@pytest.mark.parametrize("t0_str", ["12:00", "12:30", "13:00"])
-def test_apply_dropout_time(da_sample, t0_str):
-    dropout_time = pd.Timestamp(f"2024-01-01 {t0_str}")
+@pytest.mark.parametrize("t0_str", ["12:30", "13:00", "13:30"])
+def test_apply_sampled_dropout_time(da_sample, t0_str):
+    t0_time = pd.Timestamp(f"2024-01-01 {t0_str}")
+    dropout_time = t0_time + pd.Timedelta(minutes=-30)
 
-    da_dropout = apply_dropout_time(da_sample, dropout_time)
+    da_dropout = apply_sampled_dropout_time(
+        t0_time,
+        dropout_timedeltas=[pd.Timedelta(minutes=-30)],
+        dropout_frac=1.0,
+        da=da_sample,
+    )
 
     assert da_dropout.sel(time_utc=slice(None, dropout_time)).notnull().all()
     assert (
---
+++ tests/torch_datasets/test_time_slice_for_dataset.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+
+from ocf_data_sampler.torch_datasets.datasets.site import SitesDataset
+from ocf_data_sampler.torch_datasets.utils.time_slice_for_dataset import slice_datasets_by_time
+
+
+def test_time_slice_for_dataset_site_dropout(site_config_filename):
+    # Create dataset object
+    dataset = SitesDataset(site_config_filename)
+    datasets_dict = dataset.datasets_dict
+    config = dataset.config
+
+    # set dropout
+    config.input_data.site.dropout_timedeltas_minutes = [-30]
+    config.input_data.site.dropout_fraction = 1.0
+
+    sliced_datasets_dict = slice_datasets_by_time(
+        datasets_dict=datasets_dict, t0=pd.Timestamp("2023-01-01 12:00"), config=config,
+    )
+
+    site_dataset = sliced_datasets_dict["site"]
+
+    # for all 10 site ids the second element should nan due to dropout
+    assert np.all(np.isnan(site_dataset[1, :]))
+    # the last element which is after t0 should not be impacted by dropout
+    assert np.all(~np.isnan(site_dataset[-1, :]))
