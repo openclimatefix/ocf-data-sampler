c04c96df4d8c5e76f23227ec9f83815cb6a90743
Sukh-P <sukhil@hotmail.co.uk>
2025-05-19 13:52:24 +0000

    Linting

--- tests/torch_datasets/test_time_slice_for_dataset.py
+++ tests/torch_datasets/test_time_slice_for_dataset.py
@@ -1,10 +1,11 @@
-from ocf_data_sampler.torch_datasets.datasets.site import SitesDataset
-from ocf_data_sampler.torch_datasets.utils.time_slice_for_dataset import slice_datasets_by_time
 import numpy as np
 import pandas as pd
 
+from ocf_data_sampler.torch_datasets.datasets.site import SitesDataset
+from ocf_data_sampler.torch_datasets.utils.time_slice_for_dataset import slice_datasets_by_time
 
-def test_time_slice_for_dataset_site_dropout(tmp_path, site_config_filename):
+
+def test_time_slice_for_dataset_site_dropout(site_config_filename):
     # Create dataset object
     dataset = SitesDataset(site_config_filename)
     datasets_dict = dataset.datasets_dict
@@ -15,7 +16,7 @@ def test_time_slice_for_dataset_site_dropout(tmp_path, site_config_filename):
     config.input_data.site.dropout_fraction = 1.0
 
     sliced_datasets_dict = slice_datasets_by_time(
-        datasets_dict=datasets_dict, t0=pd.Timestamp("2023-01-01 12:00"), config=config
+        datasets_dict=datasets_dict, t0=pd.Timestamp("2023-01-01 12:00"), config=config,
     )
 
     site_dataset = sliced_datasets_dict["site"]
