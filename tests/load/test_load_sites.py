10748c1d5e8215aec84a7359ab87625bf8b8b475
Felix Peretz <felix@openclimatefix.org>
2025-05-23 10:37:19 +0100

    Test fix

--- tests/load/test_load_sites.py
+++ tests/load/test_load_sites.py
@@ -3,8 +3,10 @@ import xarray as xr
 from ocf_data_sampler.load.site import open_site
 
 
-def test_open_site(data_sites):
-    da = open_site(data_sites.file_path, data_sites.metadata_file_path)
+def test_open_site(default_data_site_model):
+    site_config = default_data_site_model
+    
+    da = open_site(site_config.file_path, site_config.metadata_file_path)
 
     assert isinstance(da, xr.DataArray)
     assert da.dims == ("time_utc", "site_id")
