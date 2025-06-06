642954ead6bb2fe04c0ce1744fda245d49b08da8
Felix Peretz <felix@openclimatefix.org>
2025-06-06 09:18:21 +0100

    Site load test update

--- tests/load/test_load_sites.py
+++ tests/load/test_load_sites.py
@@ -1,3 +1,5 @@
+import numpy as np
+import pandas as pd
 import xarray as xr
 
 from ocf_data_sampler.load.site import open_site
@@ -8,8 +10,27 @@ def test_open_site(default_data_site_model):
 
     assert isinstance(da, xr.DataArray)
     assert da.dims == ("time_utc", "site_id")
-
     assert "capacity_kwp" in da.coords
     assert "latitude" in da.coords
     assert "longitude" in da.coords
     assert da.shape == (49, 10)
+
+    assert np.issubdtype(da.dtype, np.floating)
+
+    assert np.issubdtype(da.coords["time_utc"].dtype, np.datetime64)
+    assert np.issubdtype(da.coords["site_id"].dtype, np.integer)
+    assert np.issubdtype(da.coords["capacity_kwp"].dtype, np.floating)
+    assert np.issubdtype(da.coords["latitude"].dtype, np.floating)
+    assert np.issubdtype(da.coords["longitude"].dtype, np.floating)
+
+    assert not da.isnull().any()
+    assert not da.coords["capacity_kwp"].isnull().any()
+    assert not da.coords["latitude"].isnull().any()
+    assert not da.coords["longitude"].isnull().any()
+
+    expected_freq = pd.to_timedelta("30 minutes")
+    time_diffs = da.coords["time_utc"].diff("time_utc")
+    if len(time_diffs) > 0:
+        assert (time_diffs == expected_freq).all()
+
+    assert len(np.unique(da.coords["site_id"])) == da.shape[1]
