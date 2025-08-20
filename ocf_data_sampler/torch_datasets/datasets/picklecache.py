8aa413bd1ec7cfe9cc0fcdb68a01497b898c5dc7
James Fulton <djamesfulton@yahoo.co.uk>
2025-08-20 13:07:31 +0000

    linting

--- ocf_data_sampler/torch_datasets/datasets/picklecache.py
+++ ocf_data_sampler/torch_datasets/datasets/picklecache.py
@@ -1,9 +1,13 @@
+"""A mixin to handle pickling and caching a dataset's state."""
+
 import os
 import pickle
 
 
 class PickleCacheMixin:
-    def __init__(self, *args, **kwargs):
+    """A mixin for classes that need to cache their state using pickle."""
+    def __init__(self, *args: list, **kwargs: dict) -> None:
+        """Initialize the pickle path and call the parent constructor."""
         self._pickle_path = None
         super().__init__(*args, **kwargs)  # cooperative multiple inheritance
 
@@ -13,17 +17,17 @@ class PickleCacheMixin:
         with open(pickle_path, "wb") as f:
             pickle.dump(self.__dict__, f)
 
-    def __getstate__(self):
+    def __getstate__(self) -> dict:
         """If presaved, only pickle reference. Otherwise pickle everything."""
         if self._pickle_path:
             return {"_pickle_path": self._pickle_path}
         else:
             return self.__dict__
 
-    def __setstate__(self, state):
+    def __setstate__(self, state: dict) -> None:
         """Restore object from pickle, reloading from presaved file if possible."""
         self.__dict__.update(state)
         if self._pickle_path and os.path.exists(self._pickle_path):
             with open(self._pickle_path, "rb") as f:
-                saved_state = pickle.load(f)
+                saved_state = pickle.load(f) # noqa: S301
                 self.__dict__.update(saved_state)
