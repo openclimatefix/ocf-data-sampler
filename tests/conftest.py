68837c4283a4caedaaa747ab46d3d21d2c53cef8
Felix Peretz <felix@openclimatefix.org>
2025-09-18 10:58:46 +0100

    tests/load neatened - module specific fixtures moved to conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -466,3 +466,42 @@ def site_config_filename(
 @pytest.fixture()
 def sites_dataset(site_config_filename):
     return SitesDataset(site_config_filename)
+
+@pytest.fixture(scope="module")
+def concatable_nwp_like_data(ds_nwp_ecmwf):
+
+    # Make a second NWP-like dataset so we can concat them
+    ds_2 = ds_nwp_ecmwf.copy(deep=True)
+    ds_2["init_time"] = pd.date_range(
+        start=ds_nwp_ecmwf.init_time.max().values + pd.Timedelta("6h"),
+        freq=pd.Timedelta("6h"),
+        periods=len(ds_nwp_ecmwf.init_time),
+    )
+
+    return ds_nwp_ecmwf, ds_2
+
+
+@pytest.fixture(scope="module")
+def nwp_like_zarr2_paths(session_tmp_path, concatable_nwp_like_data):
+
+    data_paths = [
+        f"{session_tmp_path}/nwp_like_data_{n}.zarr2" for n in range(len(concatable_nwp_like_data))
+    ]
+
+    for ds, path in zip(concatable_nwp_like_data, data_paths, strict=False):
+        ds.to_zarr(path, zarr_format=2)
+
+    return data_paths
+
+
+@pytest.fixture(scope="module")
+def nwp_like_zarr3_paths(session_tmp_path, concatable_nwp_like_data):
+
+    data_paths = [
+        f"{session_tmp_path}/nwp_like_data_{n}.zarr3" for n in range(len(concatable_nwp_like_data))
+    ]
+
+    for ds, path in zip(concatable_nwp_like_data, data_paths, strict=False):
+        ds.to_zarr(path, zarr_format=3)
+
+    return data_paths
