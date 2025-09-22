0f8a185e6ec550127ea519d8e165ff19f86ac110
ram-from-tvl <ramfromtvl@gmail.com>
2025-09-22 13:49:09 +0530

    removed some args

--- ocf_data_sampler/load/nwp/providers/utils.py
+++ ocf_data_sampler/load/nwp/providers/utils.py
@@ -57,8 +57,6 @@ def _dask_open_zarr_paths(zarr_path: str | list[str], time_dim: str, public: boo
             zarr_path,
             concat_dim=time_dim,
             combine="nested",
-            compat="no_conflicts",
-            coords="different",
             **general_kwargs,
         ).sortby(time_dim)
     else:
