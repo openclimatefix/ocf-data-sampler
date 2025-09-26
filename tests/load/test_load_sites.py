ec24650059a588bd6d28ee3b61753203dfdfb5ef
Alexandra Udaltsova <43303448+AUdaltsova@users.noreply.github.com>
2025-09-26 15:25:27 +0100

    remove unnecessary assignment

--- tests/load/test_load_sites.py
+++ tests/load/test_load_sites.py
@@ -10,8 +10,7 @@ from ocf_data_sampler.load.site import open_site
 
 def test_open_site(default_data_site_model):
     """Test the site data loader with valid data."""
-    site_model = default_data_site_model
-    da = open_site(site_model.file_path, site_model.metadata_file_path)
+    da = open_site(default_data_site_model.file_path, default_data_site_model.metadata_file_path)
 
     assert isinstance(da, xr.DataArray)
     assert da.dims == ("time_utc", "site_id")
