75d2e8fe17dad229305e26c39b743b48ad792b86
Felix Peretz <felix@openclimatefix.org>
2025-10-01 09:32:51 +0100

    Refactor of tests/select/conftest.py

--- tests/select/conftest.py
+++ tests/select/conftest.py
@@ -8,15 +8,14 @@ from tests.conftest import NWP_FREQ
 
 @pytest.fixture(scope="module")
 def da_nwp_like():
-    """Create dummy data which looks like NWP data"""
-
+    """Create dummy NWP-like data"""
     x = np.arange(-100, 100)
     y = np.arange(-100, 100)
     datetimes = pd.date_range("2024-01-02 00:00", "2024-01-03 00:00", freq=NWP_FREQ)
     steps = pd.timedelta_range("0h", "16h", freq="1h")
     channels = ["t", "dswrf"]
 
-    da_nwp = xr.DataArray(
+    return xr.DataArray(
         np.random.normal(size=(len(datetimes), len(steps), len(channels), len(x), len(y))),
         coords={
             "init_time_utc": (["init_time_utc"], datetimes),
@@ -26,32 +25,27 @@ def da_nwp_like():
             "y_osgb": (["y_osgb"], y),
         },
     )
-    return da_nwp
 
 
 @pytest.fixture(scope="module")
 def da_sample():
-
+    """Create dummy time-series sample data"""
     datetimes = pd.date_range("2024-01-01 12:00", "2024-01-01 13:00", freq="5min")
-
-    da_sat = xr.DataArray(
+    return xr.DataArray(
         np.random.normal(size=(len(datetimes),)),
         coords={"time_utc": (["time_utc"], datetimes)},
     )
-    return da_sat
 
 
 @pytest.fixture(scope="module")
 def da():
-    # Create dummy data
+    """Create dummy 2D spatial data"""
     x = np.arange(-100, 100)
     y = np.arange(-100, 100)
-
-    da = xr.DataArray(
+    return xr.DataArray(
         np.random.normal(size=(len(x), len(y))),
         coords={
             "x_osgb": (["x_osgb"], x),
             "y_osgb": (["y_osgb"], y),
         },
     )
-    return da
