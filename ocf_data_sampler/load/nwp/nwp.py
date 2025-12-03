6fd0252893bdcf4f30b8df4c8364baff2c39552d
Sukh-P <sukhil@hotmail.co.uk>
2025-12-03 18:00:04 +0000

    Linting

--- ocf_data_sampler/load/nwp/nwp.py
+++ ocf_data_sampler/load/nwp/nwp.py
@@ -5,10 +5,10 @@ import xarray as xr
 
 from ocf_data_sampler.load.nwp.providers.cloudcasting import open_cloudcasting
 from ocf_data_sampler.load.nwp.providers.ecmwf import open_ifs
+from ocf_data_sampler.load.nwp.providers.gdm import open_gdm
 from ocf_data_sampler.load.nwp.providers.gfs import open_gfs
 from ocf_data_sampler.load.nwp.providers.icon import open_icon_eu
 from ocf_data_sampler.load.nwp.providers.ukv import open_ukv
-from ocf_data_sampler.load.nwp.providers.gdm import open_gdm
 
 
 def _validate_nwp_data(data_array: xr.DataArray, provider: str) -> None:
