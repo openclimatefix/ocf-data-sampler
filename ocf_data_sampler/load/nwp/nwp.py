11b222b9a62e3cc535e87f5ab6ba6d711b148f41
Sukhil Patel <42407101+Sukh-P@users.noreply.github.com>
2025-12-15 15:07:45 +0000

    Add gdm loader (#379)

--- ocf_data_sampler/load/nwp/nwp.py
+++ ocf_data_sampler/load/nwp/nwp.py
@@ -5,6 +5,7 @@ import xarray as xr
 
 from ocf_data_sampler.load.nwp.providers.cloudcasting import open_cloudcasting
 from ocf_data_sampler.load.nwp.providers.ecmwf import open_ifs
+from ocf_data_sampler.load.nwp.providers.gdm import open_gdm
 from ocf_data_sampler.load.nwp.providers.gfs import open_gfs
 from ocf_data_sampler.load.nwp.providers.icon import open_icon_eu
 from ocf_data_sampler.load.nwp.providers.ukv import open_ukv
@@ -98,6 +99,8 @@ def open_nwp(
         _open_nwp = open_ifs
     elif provider == "icon-eu":
         _open_nwp = open_icon_eu
+    elif provider == "gencast":
+        _open_nwp = open_gdm
     elif provider == "gfs":
         _open_nwp = open_gfs
         # GFS has a public/private flag
