236bdb7a7cc7d2b089dc7dd56b4ee8d8dcceafb0
James Fulton <djamesfulton@yahoo.co.uk>
2025-12-22 17:38:58 +0000

    Add function to calculate t0 sin-cos embedding

--- ocf_data_sampler/numpy_sample/datetime_features.py
+++ ocf_data_sampler/numpy_sample/datetime_features.py
@@ -27,3 +27,42 @@ def encode_datetimes(datetimes: pd.DatetimeIndex) -> NumpySample:
         "time_sin": np.sin(time_in_radians),
         "time_cos": np.cos(time_in_radians),
     }
+
+
+def get_t0_sin_cos_embeddings(t0: pd.Timestamp, periods: list[str]) -> dict[str, np.ndarray]:
+    """Creates dictionary of sin and cos t0 time embeddings.
+
+    Args:
+        t0: The time to create radian embeddings for
+        periods: List of periods to cos-sin encode (e.g., "1h", "Nh", "1y", "Ny")
+    """
+
+    period_fracs = []
+    for period_str in periods:
+
+        if period_str.endswith("h"):
+            period_hours = int(period_str.removesuffix("h"))
+            if not period_hours>0:
+                raise ValueError("The period in hours must be >0")
+            frac = (t0.hour + t0.minute / 60) / period_hours
+
+        elif period_str.endswith("y"):
+            period_years = int(period_str.removesuffix("y"))
+            if not period_years > 0:
+                raise ValueError("The period in years must be >0")
+            days_in_year = 366 if t0.is_leap_year else 365
+            frac = (((t0.dayofyear-1) / days_in_year) + t0.year % period_years) / period_years
+        
+        else:
+            raise ValueError(f"Invalid period format: {period_str}")
+        
+        period_fracs.append(frac)
+
+    period_radians = 2 * np.pi * np.array(period_fracs, dtype=np.float32)
+
+    # Interleave Sin/Cos (using the slicing method)
+    t0_encodings = np.empty(len(periods)*2, dtype=np.float32)
+    t0_encodings[::2] = np.sin(period_radians)
+    t0_encodings[1::2] = np.cos(period_radians)
+    
+    return {"t0_encodings": t0_encodings}
--- tests/numpy_sample/test_datetime_features.py
+++ tests/numpy_sample/test_datetime_features.py
@@ -1,7 +1,10 @@
 import numpy as np
 import pandas as pd
 
-from ocf_data_sampler.numpy_sample.datetime_features import encode_datetimes
+from ocf_data_sampler.numpy_sample.datetime_features import (
+    encode_datetimes,
+    get_t0_sin_cos_embeddings,
+)
 
 
 def test_encode_datetimes():
@@ -16,3 +19,49 @@ def test_encode_datetimes():
     # Values should be between -1 and 1
     for key in ("date_sin", "date_cos", "time_sin", "time_cos"):
         assert np.all(np.abs(features[key]) <= 1)
+
+
+def test_get_t0_sin_cos_embeddings():
+
+    def check(t0s, period_strs, xs, period_floats):
+        # Test the results are expected for each t0 time
+        for x, t0 in zip(xs, t0s):
+            results = get_t0_sin_cos_embeddings(t0, periods=period_strs)["t0_encodings"]
+
+            expected_results = []
+            for p in period_floats:
+                expected_results.extend([np.sin(2*np.pi*(x / p)), np.cos(2*np.pi*(x / p))])
+            expected_results = np.array(expected_results)
+            
+            if not np.allclose(results, expected_results, atol=1e-6):
+                raise ValueError(f"{results}!={expected_results}")
+
+    # Define some t0 times and periods to check
+    t0s = pd.date_range("2024-01-01 00:00", "2024-01-01 06:00")
+    period_strs = ["1h", "2h", "6h"]
+
+    # Equivalent times and periods in float form
+    xs = np.linspace(0, 6, num=len(t0s))
+    period_floats = [1,2,6]
+
+    check(t0s, period_strs, xs, period_floats)
+
+    # Repeat the check focusing on year periods rather than hours
+    t0s = pd.to_datetime(
+        [
+            "2020-01-01 00:00", "2020-01-01 23:30", "2020-01-02 00:00",
+            "2020-06-10 00:00", "2021-01-01 00:00", "2021-01-02 00:00",
+        ]
+    )
+    period_strs = ["1y", "2y"]
+
+    # Equivalent times and periods in float form
+    # Note:
+    # - When doing year encoding we don't consider time of day
+    # - 2020 is a leap year but 2021 is not
+    # - 2020-06-10 is the 162nd day of that year
+    xs = np.array([0, 0, 1/366, 161/366, 1, 1+1/365], dtype=np.float32)
+    period_floats = [1, 2]
+
+    check(t0s, period_strs, xs, period_floats)
+
