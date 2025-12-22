8ee6c66c6cd6388c12497568531fce9dcc4430b6
James Fulton <41546094+dfulu@users.noreply.github.com>
2025-12-22 15:16:15 +0000

    Remove redundant location projection code (#384)

--- ocf_data_sampler/torch_datasets/utils/add_alterate_coordinate_projections.py
+++ ocf_data_sampler/torch_datasets/utils/add_alterate_coordinate_projections.py
@@ -9,22 +9,17 @@ from ocf_data_sampler.select.geospatial import convert_coordinates, find_coord_s
 def add_alterate_coordinate_projections(
     locations: list[Location],
     datasets_dict: dict,
-    primary_coords: str = "lon_lat",
 ) -> list[Location]:
     """Add (in-place) coordinate projections for all dataset to a set of locations.
 
     Args:
         locations: A list of locations
         datasets_dict: The dataset dict to add projections for
-        primary_coords: The primary coords of the locations
 
     Returns:
         List of locations with all coordinate projections added
     """
-    if primary_coords != "lon_lat":
-        raise ValueError("Only lon_lat locations are currently supported")
-
-    xs, ys = np.array([loc.in_coord_system(primary_coords) for loc in locations]).T
+    xs, ys = np.array([loc.in_coord_system("lon_lat") for loc in locations]).T
 
     datasets_list = []
     if "nwp" in datasets_dict:
@@ -32,7 +27,7 @@ def add_alterate_coordinate_projections(
     if "sat" in datasets_dict:
         datasets_list.append(datasets_dict["sat"])
 
-    computed_coord_systems = {primary_coords}
+    computed_coord_systems = {"lon_lat"}
 
     # Find all the coord systems required by all datasets
     for da in datasets_list:
@@ -49,7 +44,7 @@ def add_alterate_coordinate_projections(
             new_xs, new_ys = convert_coordinates(
                 x=xs,
                 y=ys,
-                from_coords=primary_coords,
+                from_coords="lon_lat",
                 target_coords=coord_system,
                 area_string=area_string,
             )
@@ -59,19 +54,5 @@ def add_alterate_coordinate_projections(
                 loc.add_coord_system(x, y, coord_system)
 
             computed_coord_systems.add(coord_system)
-
-    # Add lon-lat to start since it is required to compute the solar coords
-    if "lon_lat" not in computed_coord_systems:
-        new_xs, new_ys = convert_coordinates(
-            x=xs,
-            y=ys,
-            from_coords=primary_coords,
-            target_coords="lon_lat",
-            area_string=None,
-        )
-
-        # Add the projection to the locations objects
-        for x, y, loc in zip(new_xs, new_ys, locations, strict=False):
-            loc.add_coord_system(x, y, "lon_lat")
 
     return locations
