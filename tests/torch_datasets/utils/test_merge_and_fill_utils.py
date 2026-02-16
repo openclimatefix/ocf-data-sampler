ffbc29ca3f5289c67268b360e9d130efbf1b5727
Yuvraaj <ynarula678@gmail.com>
2026-02-16 17:43:42 +0530

    chore : removed redundant enums from test

--- tests/torch_datasets/utils/test_merge_and_fill_utils.py
+++ tests/torch_datasets/utils/test_merge_and_fill_utils.py
@@ -1,9 +1,6 @@
 import numpy as np
 
 from ocf_data_sampler.config import load_yaml_configuration
-from ocf_data_sampler.numpy_sample.generation import GenerationSampleKey
-from ocf_data_sampler.numpy_sample.nwp import NWPSampleKey
-from ocf_data_sampler.numpy_sample.satellite import SatelliteSampleKey
 from ocf_data_sampler.torch_datasets.utils.merge_and_fill_utils import (
     fill_nans_in_arrays,
     merge_dicts,
@@ -45,24 +42,26 @@ def test_fill_nans_on_numpy_samples(config_filename):
     """Test the fill_nans_in_arrays function from configuration"""
 
     configuration = load_yaml_configuration(config_filename)
-    # set custom satellite and nwp values, generation can be left as default 0.0
+    # setting custom satellite and nwp values
+    # generation can be left as default 0.0
     configuration.input_data.satellite.dropout_value = -1.0
     configuration.input_data.nwp["ukv"].dropout_value = -2.0
 
     array_with_nans = np.array([1.0, np.nan, 3.0, np.nan])
     # we use copy() to ensure separate arrays for each key
+    # using string keys directly since sample key enums have been removed
     dict = {
-        GenerationSampleKey.generation: array_with_nans.copy(),
-        SatelliteSampleKey.satellite_actual: array_with_nans.copy(),
+        "generation": array_with_nans.copy(),
+        "satellite_actual": array_with_nans.copy(),
         "ukv": {
-            NWPSampleKey.nwp: np.array([np.nan, 2.0, np.nan, 4.0]),
+            "nwp": np.array([np.nan, 2.0, np.nan, 4.0]),
         },
     }
 
     result = fill_nans_in_arrays(dict, config=configuration)
 
-    assert np.array_equal(result[GenerationSampleKey.generation], np.array([1.0, 0.0, 3.0, 0.0]))
+    assert np.array_equal(result["generation"], np.array([1.0, 0.0, 3.0, 0.0]))
     assert np.array_equal(
-        result[SatelliteSampleKey.satellite_actual], np.array([1.0, -1.0, 3.0, -1.0]),
+        result["satellite_actual"], np.array([1.0, -1.0, 3.0, -1.0]),
     )
-    assert np.array_equal(result["ukv"][NWPSampleKey.nwp], np.array([-2.0, 2.0, -2.0, 4.0]))
+    assert np.array_equal(result["ukv"]["nwp"], np.array([-2.0, 2.0, -2.0, 4.0]))
