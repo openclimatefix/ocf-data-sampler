dbd78eda19d9bff3291fe1fbee2a5093d313ef08
James Fulton <djamesfulton@yahoo.co.uk>
2026-03-23 18:01:34 +0000

    update for new satellite format

--- ocf_data_sampler/load/satellite.py
+++ ocf_data_sampler/load/satellite.py
@@ -1,6 +1,7 @@
 """Satellite loader."""
 import numpy as np
 import xarray as xr
+import json
 
 from ocf_data_sampler.load.open_xarray_tensorstore import open_zarr, open_zarrs
 from ocf_data_sampler.load.utils import (
@@ -23,24 +24,32 @@ def open_sat_data(zarr_path: str | list[str]) -> xr.DataArray:
     else:
         ds = open_zarr(zarr_path)
 
-    check_time_unique_increasing(ds.time)
+    rename_dict = {
+        "variable": "channel",
+        "time": "time_utc",
+    }
 
-    ds = ds.rename(
-        {
-            "variable": "channel",
-            "time": "time_utc",
-        },
-    )
+    for old_name, new_name in list(rename_dict.items()):
+        if old_name not in ds:
+            if new_name in ds:
+                del rename_dict[old_name]
+            else:
+                raise KeyError(f"Expected either '{old_name}' or '{new_name}' to be in dataset")
+    ds = ds.rename(rename_dict)
 
     check_time_unique_increasing(ds.time_utc)
     ds = make_spatial_coords_increasing(ds, x_coord="x_geostationary", y_coord="y_geostationary")
     ds = ds.transpose("time_utc", "channel", "x_geostationary", "y_geostationary")
 
-    data_array = get_xr_data_array_from_xr_dataset(ds)
+    da = get_xr_data_array_from_xr_dataset(ds)
+
+    # Copy the area attribute if missing
+    if "area" not in da.attrs:
+        da.attrs["area"] = json.dumps(ds.attrs["area"])
 
     # Validate data types directly loading function
-    if not np.issubdtype(data_array.dtype, np.number):
-        raise TypeError(f"Satellite data should be numeric, not {data_array.dtype}")
+    if not np.issubdtype(da.dtype, np.number):
+        raise TypeError(f"Satellite data should be numeric, not {da.dtype}")
 
     coord_dtypes = {
         "time_utc": np.datetime64,
@@ -50,8 +59,8 @@ def open_sat_data(zarr_path: str | list[str]) -> xr.DataArray:
     }
 
     for coord, expected_dtype in coord_dtypes.items():
-        if not np.issubdtype(data_array.coords[coord].dtype, expected_dtype):
-            dtype = data_array.coords[coord].dtype
+        if not np.issubdtype(da.coords[coord].dtype, expected_dtype):
+            dtype = da.coords[coord].dtype
             raise TypeError(f"{coord} should be {expected_dtype.__name__}, not {dtype}")
 
-    return data_array
+    return da
