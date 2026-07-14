6f1b6bff5aeb1347fb7fa080458b360056790fcc
James Fulton <djamesfulton@yahoo.co.uk>
2026-07-14 09:56:34 +0000

    lint

--- tests/config/test_load.py
+++ tests/config/test_load.py
@@ -1,5 +1,5 @@
-from ocf_data_sampler.config.model import Configuration
 from ocf_data_sampler.config.load import load_yaml_configuration
+from ocf_data_sampler.config.model import Configuration
 
 
 def test_load_yaml_configuration(config_filename):
