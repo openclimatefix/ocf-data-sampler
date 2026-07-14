4bf44742968f5cdc2cb1bac73ecb4eceb8d6dfa4
James Fulton <djamesfulton@yahoo.co.uk>
2026-07-14 10:20:06 +0000

    lint

--- tests/common/test_xr_tensorstore.py
+++ tests/common/test_xr_tensorstore.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import pytest
 import xarray as xr
+
 from ocf_data_sampler.common.xr_tensorstore import open_zarr, open_zarrs
 
 
--- tests/datasets/pvnet/test_materialise.py
+++ tests/datasets/pvnet/test_materialise.py
@@ -1,8 +1,8 @@
 import dask.array
 import numpy as np
 import xarray as xr
-from ocf_data_sampler.common.xr_tensorstore import open_zarr
 
+from ocf_data_sampler.common.xr_tensorstore import open_zarr
 from ocf_data_sampler.datasets.pvnet.materialise import load, load_data_dict
 
 
