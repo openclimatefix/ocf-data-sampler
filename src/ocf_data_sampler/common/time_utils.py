eba59197f0f94d6fd676c975b45dfa9d243faace
James Fulton <djamesfulton@yahoo.co.uk>
2026-07-21 13:18:22 +0000

    Update pyproject for ty type-checking

--- src/ocf_data_sampler/common/time_utils.py
+++ src/ocf_data_sampler/common/time_utils.py
@@ -72,8 +72,7 @@ def get_day_fraction(
         datetimes: the datetimes to get day fraction for
     """
     day_start = datetimes.astype("datetime64[D]")
-    elapsed = cast("Any", datetimes - day_start)
-    day_fraction = (elapsed / np.timedelta64(1, "D")).astype(np.float64)
+    day_fraction = ((datetimes - day_start) / np.timedelta64(1, "D")).astype(np.float64)
     return cast("np.float64 | NDArray[np.float64]", day_fraction)
 
 
